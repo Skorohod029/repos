dbd49304135040f24012f3de4502bb02819a39d5
Skorohod029 <113640607+Skorohod029@users.noreply.github.com>
2022-09-16 20:58:51 +0300

    Add files via upload

---
+++ LB3.1.py
@@ -0,0 +1,7 @@
+import math
+first_num = int(input("Введіть перше число: "))
+second_num = int(input("Введіть друге число: "))
+a = first_num // 10
+b = second_num // 10
+result = a * b
+print(result)
---
+++ LB3.2.py
@@ -0,0 +1,6 @@
+first = int(input("Введіть перший кут"))
+second = int(input("Введіть другий кут"))
+three = int(input("Введіть третій кут"))
+four = int(input("Введіть четвертий кут"))
+result = 540 - first - second - three - four
+print(result)
